7ad98a60faecdf15f63638823950b8b3ae13387e
Kelechi <kelechiuma@gmail.om>
2021-11-24 15:30:21 +0100

    share field, feed component styled

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { MoreVert } from '@material-ui/icons';
 
 import './post.css';
 
 export default function Post() {
-  return <div>Post</div>;
+  return (
+    <div className='post'>
+      <div className='postWrapper'>
+        <div className='postTop'>
+          <div className='postTopLeft'>
+            <img
+              className='postProfileImg'
+              src='/assets/person/1.jpeg'
+              alt=''
+            />
+            <span className='postUsername'>Sarah William</span>
+            <span className='postDate'>5 mins ago</span>
+          </div>
+          <div className='postTopRight'>
+            <MoreVert />
+          </div>
+        </div>
+        <div className='postCenter'>
+          <span className='postText'>
+            Hello Friendzone, this is my first post
+          </span>
+          <img className='postImg' src='/assets/post/1.jpeg' alt='' />
+        </div>
+        <div className='postBottom'>
+          <div className='postBottomLeft'>
+            <img className='likeIcon' src='/assets/like.png' alt='' />
+            <img className='likeIcon' src='/assets/heart.png' alt='' />
+            <span className='postLikeCounter'>32 people liked it</span>
+          </div>
+          <div className='postBottomRight'>
+            <span className='postCommentText'>9 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
